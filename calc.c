5d2e45b3bd70097c4580e4f0ef5c08297acee491
Basu Talwar <basutalwar1@gmail.com>
2024-02-23 11:50:53 +0530

    addition case added

--- calc.c
+++ calc.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 int main(){
 	int a,b;
+	char op;
 	printf("Enter a and b: \n");
 	scanf("%d %d", &a, &b);
+	printf("Enter operand");
+	scanf("%c", &op);
+	switch (op){
+		case'+';
+		printf("%d + %d = %d",a,b,a+b);
+		break;
+	}
 	return 0;
 }
